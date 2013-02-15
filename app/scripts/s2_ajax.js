b58ffc734552ac1d7374d9f3e0fd877fac068962
Sean Dunn <sd9@sanger.ac.uk>
2013-02-15 17:29:42 +0000

    Rename s2_ajax

--- app/scripts/s2_ajax.js
+++ app/scripts/s2_ajax.js
@@ -11,39 +11,18 @@ define(['config', 's2_resource'], function (config, S2Resource) {
     root:  'GET'
   };
 
-function ajaxSend(action, actionPath, data, callback) {
-      $.ajax({
-        type:       actionMethods[action],
-        url:        config.apiUrl + (actionPath || ''),
-        contentType:"json",
-        dataType:   "json",
-        data:       data,
-        success:    function (json) {
-          s2.emit({
-            event:   'gotJson',
-            callback:callback,
-            data:    json
-          });
-        }
-      });
-    }
-
-  var s2ajax = {
-    send:     config.ajaxDummy || ajaxSend,
-    search:   {
-      /*
-barcodes: function (barcode){
-
-s2ajax.ajaxSend('search', S2.resources.search.all, {barcode: barcode}, S2.resources.add);
-    }
-    */
-  },
-  parseJson:function (json) {
-    var key, resource;
-    for (key in json) {
-      resource = new S2resource(key, json[key]);
-    }
+  function ajaxSend(action, actionPath, data, callback) {
+    $.ajax({
+      type:         actionMethods[action],
+      url:          config.apiUrl + (actionPath || ''),
+      contentType:  "json",
+      dataType:     "json",
+      data:         data
+    });
   }
-};
-return s2ajax;
+
+  return {
+    send: config.ajaxDummy || ajaxSend
+  };
+
 });
