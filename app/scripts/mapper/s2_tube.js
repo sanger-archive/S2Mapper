f4d3d27505528839eaafe2d8e44f5340378d6985
Sean Dunn <sd9@sanger.ac.uk>
2013-02-18 14:17:49 +0000

    S2Tube return a tubePromise

--- app/scripts/mapper/s2_tube.js
+++ app/scripts/mapper/s2_tube.js
@@ -1,14 +1,27 @@
-// This factory creates a callback functions
-define([], function(){
+// Returns a promise that provides a tube
+define(['mapper/s2_ajax'], function(S2Ajax){
   'use strict';
+  var s2_ajax = new S2Ajax();
 
-  return function(tube){
+  return function(uuid){
+    var tubeDeferred = $.Deferred();
 
-    // This callback returns nothing but modifies tube
-    return function(response){
-      tube.rawJson      = response.responseText;
-      tube.resourceType = 'tube';
-    };
+    s2_ajax.send('read', '/tubes/' + uuid).
+      done(function(response){
+      var rawJson          = response.responseText;
+      var tubeResource     = Object.create(null);
+      tubeResource.rawJson = rawJson;
+
+      for (var action in rawJson.tube.actions) {
+        tubeResource[action] = function (sendData) {
+          s2ajax.send(action, rawJson.tube.actions[action], sendData);
+        };
+      }
+
+      tubeDeferred.resolve(tubeResource);
+    });
+
+    return tubeDeferred.promise();
   };
 
 });
--- test/spec/s2_tube_spec.js
+++ test/spec/s2_tube_spec.js
@@ -4,26 +4,23 @@ require(['config', 'mapper/s2_tube', 'json/dna_only_extraction'],function(config
   config.testJSON = testJSON.stage1;
 
   describe('S2 Tube', function(){
-    var tube = {};
+    var rawTubeJSON = config.testJSON['/tubes/11111111-2222-3333-4444-555555555555'];
 
-    var rawTube = config.testJSON['/tubes/11111111-2222-3333-4444-555555555555']
+    var s2_tube;
 
-    var dummyResponse = {
-      responseText: rawTube
-    };
+    var tubePromise = new S2Tube('11111111-2222-3333-4444-555555555555');
 
-    var tubeCallback = new S2Tube(tube);
-
-    tubeCallback(dummyResponse);
-
-    it('has a rawJson attribute', function(){
-      expect(tube.rawJson).toBeDefined();
-    });
+    // .done() sets a tube through a side effect
+    tubePromise.done(function(s2tube){ s2_tube = s2tube; });
 
     it('rawJson matches the JSON returned by S2',function(){
-      expect(tube.rawJson).toBe(rawTube);
+      expect(s2_tube.rawJson).toBe(rawTubeJSON);
     });
 
+    for (var action in rawTubeJSON.tube.actions){
+      it('has a: '+action+' action method matching the raw JSON action attribute', function(){
+        expect(s2_tube[action]).toBeDefined();
+      });
+    }
   });
-
 });
