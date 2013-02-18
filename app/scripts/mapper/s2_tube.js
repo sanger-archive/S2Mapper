d49c7014278db45f613c1e31ad51e67d1170a52c
Sean Dunn <sd9@sanger.ac.uk>
2013-02-18 10:11:03 +0000

    Fix tube

--- app/scripts/mapper/s2_tube.js
+++ app/scripts/mapper/s2_tube.js
@@ -1,17 +1,14 @@
-// The constructor is designed to be passed as a callback to an
-// Ajax promise.
+// This factory creates a callback functions
 define([], function(){
   'use strict';
 
   return function(tube){
-  return function(response){
 
-    tube.rawJson = response.responseText;
-    tube.resourceType = 'tube';
-
-    return tube;
-  };
-
+    // This callback returns nothing but modifies tube
+    return function(response){
+      tube.rawJson      = response.responseText;
+      tube.resourceType = 'tube';
+    };
   };
 
 });
