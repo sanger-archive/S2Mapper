d0906c7480e8aa8c7b846f46b00847350ee63c7f
Sean Dunn <sd9@sanger.ac.uk>
2013-03-02 15:21:48 +0000

    Renamed s2_ajax to s2ajax

--- app/scripts/mapper/s2_resource.js
+++ app/scripts/mapper/s2_resource.js
@@ -1,7 +1,7 @@
 define([ 'mapper/s2_tube_resource', 'mapper/s2_order_resource', 'mapper/s2_ajax' ], function (TubeResource, OrderResource, S2Ajax) {
   "use strict";
 
-  var s2_ajax = new S2Ajax();
+  var s2ajax = new S2Ajax();
 
   var resourceClass = {
     tube:   TubeResource,
@@ -10,21 +10,24 @@ define([ 'mapper/s2_tube_resource', 'mapper/s2_order_resource', 'mapper/s2_ajax'
 
   // Constructor function
   var ResourcePromise = function(uuid, sendAction, data){
-    var resourceDeferred = $.Deferred();
 
-    s2_ajax.send(
-      sendAction || 'read',
-      '/' + (uuid || ''),
-      data
-    ).done(function(response){
+    var resourceProcessor = function(response){
       var resourceType = Object.keys(response.responseText)[0];
-      var resClass = resourceClass[resourceType];
-      var resource = resClass.create({
+      var resClass     = resourceClass[resourceType];
+      var resource     = resClass.create({
         rawJson: response.responseText
       });
 
       resourceDeferred.resolve(resource);
-    });
+    };
+
+    var resourceDeferred = $.Deferred();
+
+    s2ajax.send(
+      sendAction || 'read',
+      '/' + (uuid || ''),
+      data
+    ).done(resourceProcessor);
 
     // Calling promise makes the defferd object readonly
     return resourceDeferred.promise();
