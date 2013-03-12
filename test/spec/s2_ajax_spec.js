b436994bfac9cb6fb835f55a97fab577c9f1286e
Sean Dunn <sd9@sanger.ac.uk>
2013-03-12 11:44:16 +0000

    Cleaned unused WIP from s2_ajax_spec

--- test/spec/s2_ajax_spec.js
+++ test/spec/s2_ajax_spec.js
@@ -1,8 +1,7 @@
 define (['config',
   'mapper/s2_ajax',
   'text!json/dna_and_rna_manual_extraction_1.json',
-  'text!json/root_data_1.json'
-], function (config, S2Ajax, testJSON_stage1, rootData_stage1) {
+], function (config, S2Ajax, testJSON_stage1) {
   'use strict';
   //load appropriate JSON for this workflow
   // config.testJSON = $.parseJSON (testJSON);
@@ -32,27 +31,6 @@ define (['config',
   }
 
 
-  //set up the tests with regard to the
-  // var jasmineTests;
-  // jasmineTests = {
-  //   1:{
-  //     1:function () {
-  //       //create the S2.findByEan13Barcode to prevent page failing
-  //       var S2Tube = {
-  //         findByEan13Barcode:function (barcode) {
-  //           return s2ajax.send ('read', '/searches', {data:barcode});
-  //         }
-  //       };
-  //       return S2Tube.findByEan13Barcode ('XX333333K').done (function (returnedResource) {
-  //         return returnedResource;
-  //       })
-  //     },
-  //     2:function () {
-  //       //this will obviously pass, we are returning the JSON it's checking against!!
-  //       return config.testJSON[1].steps[2].response;
-  //     }
-  //   }
-  // };
 
   describe("S2Ajax:-", function(){
 
@@ -113,22 +91,4 @@ define (['config',
   });
 
 
-  // *** Marked as WIP.  Probably moved out to another file ***
-  //
-  // //append the tests that should return the correct data at each stage/step
-  // $.each (config.testJSON, function (stageno, stage) {
-  //   if (jasmineTests[stageno]) {
-  //     describe (stage.description, function () {
-  //       $.each (stage.steps, function (stepno, step) {
-  //         if (jasmineTests[stageno][stepno]) {
-  //           var actual = jasmineTests[stageno][stepno] (),
-  //             expected = config.testJSON[stageno].steps[stepno].response;
-  //           it ('Matches stage ' + stageno + ', step ' + stepno + ' of JSON file', function () {
-  //             expect (expected).toEqual (actual);
-  //           });
-  //         }
-  //       })
-  //     });
-  //   }
-  // })
 });
