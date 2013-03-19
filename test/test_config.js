685f2fbaf072be5c283aba3dbcc4c09c1a280dec
TB <tim.bowcock@lenduz.com>
2013-03-19 16:31:39 +0000

    Return empty tube search

--- test/test_config.js
+++ test/test_config.js
@@ -1,10 +1,10 @@
-define([], function() {
+define(['text!json/unit/empty_tube_search.json'], function(emptyTubeData) {
   'use strict';
 
   var config = {
     apiUrl:'', // NOT USED IN TESTING
     setupTest: function (testData, stepNo){
-      var step = stepNo || 0
+      var step = stepNo || 0;
 
       testData = $.parseJSON(testData);
       config.createTestData(testData);
@@ -19,25 +19,17 @@ define([], function() {
 
     },
 
-    finalDna: {},
+    stepJson: {},
 
     createTestData: function(testData){
       var output = '';
-
       for(var stepNo in testData.steps) {
         var step = testData.steps[stepNo]
         output += '<br/>Step' + stepNo + ':' + step.description +'. Needs a ' + step.method + '. Responds with ' +
           Object.keys(step.response)[0];
-
-        config.finalDna[step.url + step.method + JSON.stringify(step.request)] = step.response;
-
+        config.stepJson[step.url + step.method + JSON.stringify(step.request)] = step.response;
       }
-
     },
-
-
-
-
     setTestJson: function(workflow){
       // Fixme Not working ideally yet still having to require
       // the package on module load.
@@ -55,14 +47,6 @@ define([], function() {
       return resultFromJson;
     },
 
-    // cpResource: function (original_uuid, new_uuid) {
-    //   var resourceJsonClone = JSON.parse(
-    //     JSON.stringify(
-    //      config.getTestJson()["/" + original_uuid]));
-
-    //   resourceJsonClone.uuid = new_uuid;
-    //   config.getTestJson ()["/" + new_uuid] = resourceJsonClone;
-    // },
 
     // Dummy out the ajax call returned by S2Ajax to test from file.
     // Returns a Deferred instead of jqXHR.
@@ -91,13 +75,26 @@ define([], function() {
       // with .done() are called as soon as they're added, which should solve
       // testing latency issues.
 
-      var response = config.finalDna[config.reqParams];
+      var response = config.stepJson[config.reqParams];
       if (response === undefined) {
         // if the stored result can't be found in the data but the url is in the root then
         // it means that the system couldn't find the data.
 
-        console.log("AJAX[" + config.reqParams + "]: not found in " + config.finalDna);
-        fakeAjaxDeferred.reject(fakeAjaxDeferred, 'error');
+        console.log("AJAX[" + config.reqParams + "]: not found in " + config.stepJson);
+          // Check whether this is a search we need to fake.
+        if (options.url === '/searches' && options.type.toLowerCase() === 'post') {
+          console.log('But we are searching for a ' + options.data.search.model  + ', so need to return the empty data');
+
+          fakeAjaxDeferred.resolve({
+            url:           options.url,
+            'status':      200,
+            responseTime:  750,
+            responseText:  JSON.parse(emptyTubeData).steps[0].response
+          });
+
+        } else {
+          fakeAjaxDeferred.reject(fakeAjaxDeferred, '404 error');
+        }
       } else {
         console.log("AJAX[" + config.reqParams + "]: responding with:");
         console.log(response);
@@ -110,7 +107,6 @@ define([], function() {
         });
 
       }
-
       return fakeAjaxDeferred;
     },
 
