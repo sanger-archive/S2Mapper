bef5d22ab638d72eac55fa70f9b491e5f55231e5
Matthew Denner <md12@sanger.ac.uk>
2013-03-22 14:54:58 +0000

    Sequential deferred helper
    Allows for sequences of deferreds to be processed linearly.
    Behaves in a similar fashion to $.Deferred.

--- app/scripts/mapper/s2_labellable.js
+++ app/scripts/mapper/s2_labellable.js
@@ -2,7 +2,9 @@
  * Something that can have labels on it is labellable.  Require this module and extend the instance
  * with the returned object to get the appropriate functionality.
  */
-define([], function() {
+define([
+  'mapper/support/sequential_deferreds'
+], function(SequentialDeferred) {
   'use strict';
 
   return {
@@ -15,18 +17,20 @@ define([], function() {
     },
 
     attachBarcode: function(contentType, intendedRole) {
-      var labellable = this, deferred = $.Deferred();
-
-      labellable.root.barcode.create({
-        labware:  labellable.resourceType,
-        contents: contentType,
-        role:     intendedRole || "Stock"
-      }).done(function(barcode) {
-        barcode.label(labellable).done(function() {
-          deferred.resolve();
-        }).fail(deferred.failure);
-      }).fail(deferred.failure);
-      return deferred.promise();
+      return SequentialDeferred({
+        labware: this
+      }).then(function(state) {
+        return state.labware.root.barcodes.create({
+          labware:  state.labware.resourceType,
+          contents: contentType,
+          role:     intendedRole || "Stock"
+        });
+      }).then(function(state, barcode) {
+        state.barcode = barcode;
+        return barcode.label(state.labware);
+      }).resolve(function(state) {
+        return state;
+      }).promise();
     }
   };
 });
