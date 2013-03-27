07b31948cf6da548e806fd742ad535048cc6974a
benoitmangili <benoit.mangili@tessella.com>
2013-03-27 10:05:40 +0000

    update on batch and tests (added more tests, fixed a bug)

--- app/scripts/mapper/s2_batch_resource.js
+++ app/scripts/mapper/s2_batch_resource.js
@@ -66,10 +66,12 @@ define(['mapper/s2_base_resource'], function (BaseResource) {
     });
 
     var ordersHashedByUUID = {};
+    var resourceUUIDs = []
     var listOfPromisesForOrders = [];
 
     // search for the *unique* orders related to the batch
     resources.map(function (rsc) {
+      resourceUUIDs.push(rsc.uuid);
       var promise = $.Deferred();
       listOfPromisesForOrders.push(promise);
       rsc.order().then(function (order) {
@@ -89,7 +91,7 @@ define(['mapper/s2_base_resource'], function (BaseResource) {
       // for each unique order
       orderUpdatePromises = orders.map(function (order) {
         // update items
-        return updateItemsInOrderWithBatch(order, createdBatch, "done");
+        return updateItemsInOrderWithBatch(order, createdBatch, resourceUUIDs, "done");
       });
 
       // then, when all updated...
@@ -104,13 +106,38 @@ define(['mapper/s2_base_resource'], function (BaseResource) {
         });
   }
 
-  function updateItemsInOrderWithBatch(order, batch, filteringStatus) {
+  function updateItemsInOrderWithBatch(order, batch, concernedLabwareUUIDs, filteringStatus) {
     // for one order, update the role status of the items, inserting the batch UUID
+    // but ONLY if the items is one of the concerned ones, ie one of the labware added to the batch
     // If a filteringStatus is given, it only applies to the role with this status.
     var updateJson = { "items":{} };
     // for each role
     _.each(order.items, function (labwares, role, list) {
       // for each labware (ie tube)
+      var labwaresInBatch = labwares.filter(function(labware){
+        // filtering the unnecessary tubes
+        return _.contains(concernedLabwareUUIDs,labware.uuid);
+      });
+      // well... for each labware concerned (ie tube added to the batch)
+      _.each(labwaresInBatch, function (labware) {
+            // we update a piece of JSON
+            if (!filteringStatus || labware.status === filteringStatus) {
+              if (!updateJson.items[role]) {
+                updateJson.items[role] = {};
+              }
+              updateJson.items[role][labware.uuid] = { "batch_uuid":batch.uuid };
+            }
+          }
+      );
+    });
+    // and then pass it to the order for update
+    return order.update(updateJson);
+  }
+
+  function updateItemsInOrderWithNewRole(order, batch, filteringStatus) {
+
+    var updateJson = { "items":{} };
+    _.each(order.items, function (labwares, role, list) {
       _.each(labwares, function (labware) {
             if (!filteringStatus || labware.status === filteringStatus) {
               if (!updateJson.items[role]) {
@@ -123,6 +150,7 @@ define(['mapper/s2_base_resource'], function (BaseResource) {
     });
     return order.update(updateJson);
   }
+
 
   var instanceMethods = {
     save:function (unsavedBatch) {
