e2ed82fec16438e6d3b82538a7eccf852a30dc9a
benoitmangili <benoit.mangili@tessella.com>
2013-04-15 15:54:26 +0100

    fixed the ajax_spec + test_config_spec to handle async testing

--- test/spec/support/test_config_spec.js
+++ test/spec/support/test_config_spec.js
@@ -1,117 +1,174 @@
 define([
-  'config'
+  'resource_test_helper'
+  , 'config'
   , 'text!/json/unit/test_config_data.json'
-], function (config, minidb) {
+], function (TestHelper, config, minidb) {
 
   'use strict';
 
-  describe("the fake ajax call", function () {
-    var put1Call = {
-      type:'PUT',
-      url:'/1',
-      dataType:"json",
-      headers:{ 'Content-Type':'application/json' },
-      data:""
-    };
-    var put2Call = {
-      type:'PUT',
-      url:'/2',
-      dataType:"json",
-      headers:{ 'Content-Type':'application/json' },
-      data:""
-    };
+  TestHelper(function (results) {
 
-    beforeEach(function () {
-      config.loadTestData(minidb);
-    });
-
-    it("changes the current stage when sending a 'PUT'.", function () {
-      spyOn(config, "progress").andCallThrough();
-      config.ajax(put1Call)
-          .then(function () {
-            return expect(config.progress).toHaveBeenCalled();
-          }).fail(function () {
-            throw "oops";
-          });
-    });
-
-
-    it("testData is complete", function () {
-      expect(config.testData["default"]).toBeDefined();
-      expect(config.testData["after"]).toBeDefined();
-      expect(config.testData["even_later"]).toBeDefined();
-    });
-
-    it("changes the hashedData when changing stage.", function () {
-      var key = "GET:/";
-      expect(config.hashedTestData[key].response.value).toEqual(0);
-      config.ajax(put1Call)
-          .then(function () {
-            expect(config.hashedTestData[key].response.value).toEqual(1);
-            return config.ajax(put1Call)
-          })
-          .then(function () {
-            expect(config.hashedTestData[key].response.value).toEqual(2);
-          }).fail(function () {
-            throw "oops";
-          });
-    });
-
-    it("changes the hashedData when changing stage, regardless of the call order.", function () {
-      var key = "GET:/";
-      expect(config.hashedTestData[key].response.value).toEqual(0);
-      config.ajax(put2Call)
-          .then(function () {
-            expect(config.hashedTestData[key].response.value).toEqual(2);
-            return config.ajax(put1Call)
-          })
-          .then(function () {
-            expect(config.hashedTestData[key].response.value).toEqual(1);
-          }).fail(function () {
-            throw "oops";
-          });
-    });
-
-    it("return an empty thing when a search fails.", function () {
-      var searchCall = {
-        type:'POST',
-        url:'/searches',
+    describe("the fake ajax call", function () {
+      var put1Call = {
+        type:'PUT',
+        url:'/1',
         dataType:"json",
         headers:{ 'Content-Type':'application/json' },
-        data:{ 'what':'something impossible to find' }
+        data:""
+      };
+      var put2Call = {
+        type:'PUT',
+        url:'/2',
+        dataType:"json",
+        headers:{ 'Content-Type':'application/json' },
+        data:""
       };
 
-      config.ajax(searchCall)
-          .then(function (response) {
-            expect(response).toBeDefined();
-            expect(response["responseText"]).toBeDefined();
-            expect(response["responseText"]["search"]).toBeDefined();
-            expect(response["responseText"]["search"]["actions"]).toBeDefined();
-            expect(response["responseText"]["search"]["actions"]["first"]).toBeDefined();
+      beforeEach(function () {
+        config.loadTestData(minidb);
+      });
 
-            var URL_FOR_NO_RESULT = response["responseText"]["search"]["actions"]["first"];
+      it("changes the current stage when sending a 'PUT'.", function () {
+        spyOn(config, "progress").andCallThrough();
+        runs(function () {
+          config.ajax(put1Call)
+              .then(results.expected)
+              .fail(results.unexpected);
+        });
+        waitsFor(results.hasFinished);
 
-            var resultCall = {
-              type:'GET',
-              url:URL_FOR_NO_RESULT,
-              dataType:"json",
-              headers:{ 'Content-Type':'application/json' },
-              data:""
-            };
+        runs(function () {
+          return expect(config.progress).toHaveBeenCalled();
+        });
 
-            return config.ajax(resultCall);
-          })
-          .then(function (response) {
-            expect(response).toBeDefined();
-            expect(response["responseText"]).toBeDefined();
-            expect(response["responseText"]["size"]).toBeDefined();
-            expect(response["responseText"]["size"]).toEqual(0);
+      });
 
-          }).fail(function () {
-            throw "oops";
-          });
+      it("testData is complete", function () {
+        runs(function () {
+          expect(config.testData["default"]).toBeDefined();
+          expect(config.testData["after"]).toBeDefined();
+          expect(config.testData["even_later"]).toBeDefined();
+        });
+      });
+
+      it("changes the hashedData when changing stage.", function () {
+        var key = "GET:/";
+        results.resetFinishedFlag();
+        expect(config.hashedTestData[key].response.value).toEqual(0);
+        runs(function () {
+          config.ajax(put1Call)
+              .then(results.expected)
+              .fail(results.unexpected);
+        });
+        waitsFor(results.hasFinished);
+
+        runs(function () {
+          expect(config.hashedTestData[key].response.value).toEqual(1);
+        });
+
+        runs(function () {
+          results.resetFinishedFlag();
+          config.ajax(put1Call)
+              .then(results.expected)
+              .fail(results.unexpected);
+        });
+        waitsFor(results.hasFinished);
+
+        runs(function () {
+          expect(config.hashedTestData[key].response.value).toEqual(2);
+        });
+      });
+
+      it("changes the hashedData when changing stage, regardless of the call order.", function () {
+        var key = "GET:/";
+        expect(config.hashedTestData[key].response.value).toEqual(0);
+
+        results.resetFinishedFlag();
+        expect(config.hashedTestData[key].response.value).toEqual(0);
+        runs(function () {
+          config.ajax(put2Call)
+              .then(results.expected)
+              .fail(results.unexpected);
+        });
+        waitsFor(results.hasFinished);
+
+        runs(function () {
+          expect(config.hashedTestData[key].response.value).toEqual(2);
+        });
+
+        runs(function () {
+          results.resetFinishedFlag();
+          config.ajax(put1Call)
+              .then(results.expected)
+              .fail(results.unexpected);
+        });
+        waitsFor(results.hasFinished);
+
+        runs(function () {
+          expect(config.hashedTestData[key].response.value).toEqual(1);
+        });
+
+      });
+
+      it("return an empty thing when a search fails.", function () {
+        var searchCall = {
+          type:'POST',
+          url:'/searches',
+          dataType:"json",
+          headers:{ 'Content-Type':'application/json' },
+          data:{ 'what':'something impossible to find' }
+        };
+
+        var responseSearchCall;
+
+        runs(function () {
+          results.resetFinishedFlag();
+          config.ajax(searchCall)
+              .then(results.assignTo('response'))
+              .then(results.expected)
+              .fail(results.unexpected);
+        });
+        waitsFor(results.hasFinished);
+
+        runs(function () {
+          responseSearchCall = results.get('response');
+          expect(responseSearchCall).toBeDefined();
+          expect(responseSearchCall["responseText"]).toBeDefined();
+          expect(responseSearchCall["responseText"]["search"]).toBeDefined();
+          expect(responseSearchCall["responseText"]["search"]["actions"]).toBeDefined();
+          expect(responseSearchCall["responseText"]["search"]["actions"]["first"]).toBeDefined();
+
+        });
+
+        runs(function () {
+
+          var URL_FOR_NO_RESULT = responseSearchCall["responseText"]["search"]["actions"]["first"];
+          var resultCall = {
+            type:'GET',
+            url:URL_FOR_NO_RESULT,
+            dataType:"json",
+            headers:{ 'Content-Type':'application/json' },
+            data:""
+          };
+
+          results.resetFinishedFlag();
+          config.ajax(resultCall)
+              .then(results.assignTo('response'))
+              .then(results.expected)
+              .fail(results.unexpected);
+        });
+        waitsFor(results.hasFinished);
+
+        runs(function () {
+          var responseNoResult = results.get('response');
+          expect(responseNoResult).toBeDefined();
+          expect(responseNoResult["responseText"]).toBeDefined();
+          expect(responseNoResult["responseText"]["size"]).toBeDefined();
+          expect(responseNoResult["responseText"]["size"]).toEqual(0);
+        });
+      });
+
     });
 
   });
-
 });
