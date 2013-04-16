0ac0cf4b45f304d961aeaaacb599ff5284d73cac
David Allen <da6@sanger.ac.uk>
2013-04-16 10:20:56 +0100

    Reverting test_main to previous version

--- test/test_main.js
+++ test/test_main.js
@@ -13,23 +13,23 @@ require.config({
 });
 
 require([
-//        'domReady!',
-//        'spec/support/test_config_spec',
-//        'spec/support/pluralization_spec',
-//        'spec/support/deferred_spec',
-//        'spec/services/print_spec',
-//        'spec/s2_ajax_spec',
-//        'spec/s2_search_resource_spec',
-//        'spec/s2_labellable_spec',
-//        'spec/s2_base_resource_spec',
-//        'spec/s2_tube_resource_spec',
-//        'spec/s2_spin_column_resource_spec',
-//        'spec/s2_order_resource_spec',
-//        'spec/s2_batch_resource_spec',
-//        'spec/s2_root_spec',
-//        'spec/s2_barcode_resource_spec',
+        'domReady!',
+        'spec/support/test_config_spec',
+        'spec/support/pluralization_spec',
+        'spec/support/deferred_spec',
+        'spec/services/print_spec',
+        'spec/s2_ajax_spec',
+        'spec/s2_search_resource_spec',
+        'spec/s2_labellable_spec',
+        'spec/s2_base_resource_spec',
+        'spec/s2_tube_resource_spec',
+        'spec/s2_spin_column_resource_spec',
+        'spec/s2_order_resource_spec',
+        'spec/s2_batch_resource_spec',
+        'spec/s2_root_spec',
+        'spec/s2_barcode_resource_spec',
         'spec/operations_spec',
-//        'spec/integration/dna_only_extraction_spec'
+        'spec/integration/dna_only_extraction_spec'
 ], function() {
   window.setTimeout(runJasmineTests, 100);
 });
