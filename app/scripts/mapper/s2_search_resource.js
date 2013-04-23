6e239e6d69afb96b0a7dc12710cf911253a8706d
benoitmangili <benoit.mangili@tessella.com>
2013-04-23 20:07:19 +0100

    moved the findByBarcode to the base_resource level

--- app/scripts/mapper/s2_search_resource.js
+++ app/scripts/mapper/s2_search_resource.js
@@ -8,11 +8,15 @@ define([
   function processor(resultModel) {
     return function(resultDeferred) {
       return function(response) {
+        if (response.responseText.size === 0){
+          // reject with error...
+          return resultDeferred.reject(resultDeferred,'Barcode not found');
+        }
+
         return resultDeferred.resolve(_.map(
           response.responseText[resultModel.resourceType.pluralize()],
           function(resourceJson) {
             var rawJson = {}; rawJson[resultModel.resourceType] = resourceJson;
-            // TODO: remove 'root' here once Ian's changes come in to fix the BaseResource reference
             return resultModel.instantiate({rawJson: rawJson});
           }
         ));
@@ -26,18 +30,49 @@ define([
       var handler = processor(resultModel);
 
       return {
-        first: searchPageHandler('first'),
-        last:  searchPageHandler('last')
+        firstPage: searchHandler('first'),
+        lastPage:  searchHandler('last'),
+        first: searchHandler('first', 0),
+        last:  searchHandler('last', -1)
       };
 
-      function searchPageHandler(actionName) {
-        return function(options) {
+      function indexError(deferred, indexOnPage,nbOfResult){
+        return deferred.reject('Index Error while retrieving '+indexOnPage+'th ' +
+            'result from page : Only '+nbOfResult+' elements')
+      }
+
+      function searchHandler(actionName, indexOnPage){
+        // returns a function which will get all the results on the specified page
+        // OR, if an indexOnPage is given,
+        // returns the 'indexOnPage'-th resource on the specified page of result.
+        // If indexOnPage is negative, the
+        return function(options){
           var deferred = $.Deferred();
-          root.searches.create(options).done(function(searchResult) {
-            searchResult[actionName](undefined, handler).done(deferred.resolve).fail(deferred.reject);
-          }).fail(deferred.reject);
+          root.searches.create(options)
+              .then(function (searchResult) {
+                return searchResult[actionName](undefined, handler);
+              })
+              .then(function(result){
+                if (indexOnPage !== undefined) {
+
+                  var nbOfResult = result.length;
+                  if (indexOnPage <0){
+                    // allowing -1 to be the last element
+                    // -2, one before the last, etc.
+                    indexOnPage = indexOnPage+nbOfResult;
+                  }
+                  if ( (indexOnPage >= nbOfResult) || (indexOnPage+nbOfResult < 0) ){
+                      return indexError(deferred,indexOnPage,nbOfResult);
+                  }
+
+                  return deferred.resolve(result[indexOnPage]);
+                } else {
+                  return deferred.resolve(result);
+                }
+              })
+              .fail(deferred.reject);
           return deferred;
-        };
+        }
       }
     }
   });
