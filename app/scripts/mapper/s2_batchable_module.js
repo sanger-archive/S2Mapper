a8257abaf5da40d0dce12677b95eeee394b75591
benoitmangili <benoit.mangili@tessella.com>
2013-05-10 14:07:06 +0100

    removed copy'n'paste leftovers....

--- app/scripts/mapper/s2_batchable_module.js
+++ app/scripts/mapper/s2_batchable_module.js
@@ -1,7 +1,3 @@
-/*
- * Something that can have labels on it is labellable.  Require this module and extend the instance
- * with the returned object to get the appropriate functionality.
- */
 define([
   'mapper/support/deferred'
 ], function(Deferred) {
