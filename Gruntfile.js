b1122e14989c61813f76f8d6b46415af85493662
Chris Smith <cs24@sanger.ac.uk>
2013-08-19 16:49:16 +0100

    Fixed build task
    CSSMin Grunt task didn't have a target so builds were failing. Pointed
    the task's files directory to the correct CSS directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -246,15 +246,15 @@ module.exports = function (grunt) {
             // `index.html` will take care of minification, e.g.
             //
             //     <!-- build:css({.tmp,app}) styles/main.css -->
-            //
-            // dist: {
-            //     files: {
-            //         '<%= yeoman.dist %>/styles/main.css': [
-            //             '.tmp/styles/{,*/}*.css',
-            //             '<%= yeoman.app %>/styles/{,*/}*.css'
-            //         ]
-            //     }
-            // }
+            
+            dist: {
+                files: {
+                    '<%= yeoman.dist %>/styles/main.css': [
+                        '.tmp/styles/{,*/}*.css',
+                        '<%= yeoman.app %>/styles/{,*/}*.css'
+                    ]
+                }
+            }
         },
         htmlmin: {
             dist: {
