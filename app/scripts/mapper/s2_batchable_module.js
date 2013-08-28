2b1854393cec85662a2b9c38f8f323e8f4e19f8a
Sean Dunn <sd9@sanger.ac.uk>
2013-08-28 16:49:02 +0100

    Add Batchable#orders()

--- app/scripts/mapper/s2_batchable_module.js
+++ app/scripts/mapper/s2_batchable_module.js
@@ -13,6 +13,34 @@ define([
         return $.Deferred();
       },
 
+      orders: function(){
+        var thisResource      = this;
+        var root = this.root;
+        if (thisResource._orders) {
+          return $.Deferred().resolve(thisResource._orders).promise();
+        }
+
+        var deferred = root.laboratorySearches.handling(root.orders).firstPage({
+          "user":       root.user,
+          "description":"search for order",
+          "model":      "order",
+          "criteria":   {
+            "item":{
+              "uuid": thisResource.uuid
+            }
+          }
+        })
+        .then(function(orders){
+          thisResource._orders = orders;
+          return orders;
+        });
+
+        return deferred.promise();
+
+      },
+
+      // TODO: Refactor code to always used paged results returned as an array.
+      // Remove this method and use orders() instead.
       // Search for Order from tube uuid.
       order: function(){
         var thisResource      = this;
@@ -24,7 +52,7 @@ define([
         } else {
           // assuming that there is only one order for one resource...
           // we use the "first" method, and not the "firstPage" one
-          thisResource.root.laboratorySearches.handling(root.orders).first({
+          root.laboratorySearches.handling(root.orders).first({
             "user":       root.user,
             "description":"search for order",
             "model":      "order",
