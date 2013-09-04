a05b51d8a21c5c709f53c021c456fad436943ab2
Sean Dunn <sd9@sanger.ac.uk>
2013-09-04 16:19:07 +0100

    Add transferBehaviour string

--- app/scripts/mapper/s2_spin_column_resource.js
+++ app/scripts/mapper/s2_spin_column_resource.js
@@ -1,16 +1,19 @@
 define([
-       'mapper/s2_base_resource',
-       'mapper/s2_labeling_module',
-       'mapper/s2_batchable_module'
+  "mapper/s2_base_resource",
+  "mapper/s2_labeling_module",
+  "mapper/s2_batchable_module"
 ], function(BaseResource, LabelingModule, BatchableModule) {
-  'use strict';
+  "use strict";
 
-  var SpinColumn = BaseResource.extendAs('spin_column', function(spinColumnInstance, options) {
+  var SpinColumn = BaseResource.extendAs("spin_column", function(spinColumnInstance, options) {
     $.extend(spinColumnInstance, instanceMethods);
     $.extend(spinColumnInstance, LabelingModule);
     return spinColumnInstance;
   });
-  SpinColumn.resourceType = 'spin_column';
+
+  SpinColumn.resourceType      = "spin_column";
+  SpinColumn.transferBehaviour = "tubeLike";
+
   var instanceMethods = BatchableModule(SpinColumn.resourceType);
   return SpinColumn;
 });
