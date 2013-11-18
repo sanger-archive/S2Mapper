bbcf8a98d4acc540e562c939317b92dce19bc2e7
Sean Dunn <sd9@sanger.ac.uk>
2013-11-18 17:50:03 +0000

    Replacing with a separate section for filter_papers - This will go back when/if filter_papers become tube-like
    This reverts commit 0dbe1dcf93e26242d3e0a5000fb46b4149a98a02.

--- app/scripts/mapper/s2_transfer_resources.js
+++ app/scripts/mapper/s2_transfer_resources.js
@@ -2,16 +2,35 @@ define(["mapper/s2_base_resource"], function(BaseResource) {
   "use strict";
 
   var transferModels = _.chain({
-    // Plate-to-plate like transfers ...
-    "transfer_plates_to_plates":                 plateToPlate,
-    "transfer_multiple_filter_papers_to_plates": plateToPlate,
+    "transfer_plates_to_plates": function(details) {
+      return {
+        amount:          details.amount,
+        aliquot_type:    details.aliquot_type,
+        source_uuid:     details.input.resource.uuid,
+        source_location: details.source_location,
+        target_uuid:     details.output.resource.uuid,
+        target_location: details.target_location
+      };
+    },
 
-    // Tube-to-tube like transfers ...
-    "transfer_tubes_to_tubes":                   tubeToTube,
+    "transfer_tubes_to_tubes": function(details) {
+      return {
+        fraction:     details.fraction,
+        aliquot_type: details.aliquot_type,
+        source_uuid:  details.input.resource.uuid,
+        target_uuid:  details.output.resource.uuid
+      };
+    },
 
-    // Plate-to-tube like transfers ...
-    "transfer_wells_to_tubes":                   plateToTube,
-    "transfer_multiple_filter_papers_to_tubes":  plateToTube
+    "transfer_wells_to_tubes": function(details) {
+      return {
+        fraction:        details.fraction,
+        aliquot_type:    details.aliquot_type,
+        source_uuid:     details.input.resource.uuid,
+        source_location: details.input.location,
+        target_uuid:     details.output.resource.uuid
+      };
+    }
   }).pairs().map(function(pair) {
     return $.extend(BaseResource.extendAs(pair[0], function(transferInstance) {
       return transferInstance;
@@ -29,34 +48,4 @@ define(["mapper/s2_base_resource"], function(BaseResource) {
       }).value();
     }
   };
-
-  function plateToTube(details) {
-    return {
-      fraction:        details.fraction,
-      aliquot_type:    details.aliquot_type,
-      source_uuid:     details.input.resource.uuid,
-      source_location: details.input.location,
-      target_uuid:     details.output.resource.uuid
-    };
-  }
-
-  function plateToPlate(details) {
-    return {
-      amount:          details.amount,
-      aliquot_type:    details.aliquot_type,
-      source_uuid:     details.input.resource.uuid,
-      source_location: details.source_location,
-      target_uuid:     details.output.resource.uuid,
-      target_location: details.target_location
-    };
-  }
-
-  function tubeToTube(details) {
-    return {
-      fraction:     details.fraction,
-      aliquot_type: details.aliquot_type,
-      source_uuid:  details.input.resource.uuid,
-      target_uuid:  details.output.resource.uuid
-    };
-  }
 });
