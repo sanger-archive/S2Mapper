4df84982265e4a2b580b8847aca9c4a7996a4da5
Eduardo Martin Rojo <emr@mib105656i.local>
2013-12-11 00:22:28 +0000

    Adds methods for scoring gel image

--- app/scripts/mapper/s2_gel_image_resource.js
+++ app/scripts/mapper/s2_gel_image_resource.js
@@ -4,15 +4,25 @@ define([
   'use strict';
 
   var GelImage = BaseResource.extendAs('gel_image', function(gelImageInstance, options) {
+    $.extend(gelImageInstance, instanceMethods);
     return gelImageInstance;
   });
+  
 
   GelImage.searchAddress = "qualitySearches";
   GelImage.resourceType = 'gel_image';
-
-  GelImage.find = function(sangerUUID){
-    return this.root.retrieve({uuid:sangerUUID, "resourceType":GelImage.resourceType, s2AppUrl:"lims-quality"});
-  };  
+ 
+  GelImage.score = function(scores) {
+    return this.root.retrieve({
+      uuid: this.uuid,
+      "resourceType": GelImage.resourceType, 
+      s2AppUrl:"lims-quality",
+      data: scores,
+      sendAction: "update"
+    });
+  };
+  
+  var instanceMethods = { score: GelImage.score};
 
   return GelImage;
 });
