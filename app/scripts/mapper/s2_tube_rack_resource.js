70c7daf0d89a9ede25e5f0a3b97927034a125d07
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2014-09-03 17:21:04 +0100

    Added configuration list of roles to check in the validation as we only want this change to take place on reracking stage (samples.rack.stock...)

--- app/scripts/mapper/s2_tube_rack_resource.js
+++ app/scripts/mapper/s2_tube_rack_resource.js
@@ -1,8 +1,9 @@
 define([
   "mapper/s2_base_resource",
   "mapper/s2_labeling_module",
-  "mapper/s2_batchable_module"
-], function(BaseResource, LabelingModule, BatchableModule){
+  "mapper/s2_batchable_module",
+  "config"
+], function(BaseResource, LabelingModule, BatchableModule, config){
   "use strict";
 
   var TubeRack = BaseResource.extendAs("tube_rack", function(tubeRackInstance, options) {
@@ -45,26 +46,33 @@ define([
       }, this)).flatten().uniq().value();
     },
     processItemOrderUpdate: function(order, itemOrderUpdate) {
-      return _.every(_.pairs(itemOrderUpdate.items, function(roleName, updateObj) {
+      var tubeRack = this;
+      return _.every(_.pairs(itemOrderUpdate.items), function(pairs) {
+        var roleName = pairs[0], updateObj = pairs[1];
+        if (!_.isUndefined(config) && !_.isUndefined(config.resetableRolesList) &&
+          _.indexOf(config.resetableRolesList, roleName)<0) {
+          // If it is not a role to be checked
+          return true;
+        }
         // If my rack is going to be marked as "unused" in a role for the order
-        if ((!_.isUndefined(updateObj[this.uuid])) && (updateObj[this.uuid].event === "unuse")) {
+        if ((!_.isUndefined(updateObj[tubeRack.uuid])) && (updateObj[tubeRack.uuid].event === "unuse")) {
           // Check that there isn't any tubes remaining inside my rack that belongs to this order that are
           // identified inside this order as an enabled item (status==done) because in that case they would be still in use.
           // If this is the case, I mustn't execute the "unuse" event on this rack for this order, as I 
           // am still having tubes awaiting to be processed among my contents.
-          var uuidForRemainingTubes = _.chain(this.tubes).values().pluck("uuid").value();
+          var uuidForRemainingTubes = _.chain(tubeRack.tubes).values().pluck("uuid").value();
           var uuidForEnabledItemsOnOrder = _.chain(order.items).values().flatten().
           filter(function(item) {
             return item.status=='done';
           }).pluck("uuid").value();
           var allowUpdate =  (_.difference(uuidForRemainingTubes, uuidForEnabledItemsOnOrder).length === 0);
           if (!allowUpdate) {
-            delete updateObj[this.uuid];
+            delete updateObj[tubeRack.uuid];
           }
           return allowUpdate;
         }
         return true;
-      }));
+      });
     }
   };
 
